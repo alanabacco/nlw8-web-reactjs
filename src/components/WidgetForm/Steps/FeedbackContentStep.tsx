6fdf7ddda88128c7261cb05f27ddc65d159ec3db
Alana Bacco <alana.bacco@lambda3.com.br>
2022-05-09 00:15:09 -0300

    cria botão de screenshot

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,4 +1,5 @@
 import { ArrowLeft } from "phosphor-react";
+import { useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
 import CloseButton from "../../CloseButton";
 import ScreenshotButton from "../ScreenshotButton";
@@ -8,10 +9,13 @@ interface FeedbackContentStepProps {
   onFeedbackRestartRequested: () => void;
 }
 
+
+
 function FeedbackContentStep({
   feedbackType,
   onFeedbackRestartRequested,
 }: FeedbackContentStepProps) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);  
   const FeedbackTypeInfo = feedbackTypes[feedbackType];
 
   return (
@@ -41,7 +45,7 @@ function FeedbackContentStep({
           placeholder="Conte com detalhes o que está acontecendo..."
         />
         <footer className="flex gap-2 mt-2">
-          <ScreenshotButton />     
+          <ScreenshotButton screenshot={screenshot} onScreenshotTook={setScreenshot} />
           <button
             type="submit"
             className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex-justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
